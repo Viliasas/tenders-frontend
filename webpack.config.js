2f561079f43723f9801238afe1cf5ae56cd5f3ac
Vilius Mačiulis <Viliasas@users.noreply.github.com>
2020-10-31 21:35:22 +0200

    Added initial WebPack configuration

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+// Import dependencies
+const path = require('path');
+const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
+
+module.exports = (env) => {
+    return {
+        watch: env.watch === 'true',
+        mode: env.mode,
+        entry: './app/app.module.js',
+        output: {
+            filename: 'app.bundle.js',
+            path: path.resolve(__dirname, 'dist'),
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env']
+                        }
+                    }
+                },
+                {
+                    test: /\.html$/,
+                    loader: 'html',
+                },
+                {
+                    test: /\.css$/,
+                    use: [
+                        'style-loader',
+                        'css-loader',
+                    ],
+                },
+            ]
+        },
+        devtool: 'inline-source-map',
+        plugins: [
+            new FriendlyErrorsWebpackPlugin(),
+        ],
+    };
+}
